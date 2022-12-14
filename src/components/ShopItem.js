fb97f4d08b225e57ee0ab404ba0be7ab153af6e2
Rachael Cole <jcrachael@gmail.com>
2022-12-14 20:58:15 +1100

    Added ShopItem component and basic CSS styles for Shop route

---
+++ src/components/ShopItem.js
@@ -0,0 +1,15 @@
+import "../styles/ShopItem.css";
+
+export default function ShopItem({ id, title, price, img }) {
+  return (
+    <div className="ShopItem" id={`item-` + id}>
+      <img src={img} alt={title}></img>
+      <span className="caption">
+        <h3 className="item-title">{title}</h3>
+        <p className="price">{price}</p>
+      </span>
+
+      <p className="add">Add to cart</p>
+    </div>
+  );
+}
--- src/routes/Shop.js
+++ src/routes/Shop.js
@@ -1,3 +1,39 @@
+import searchIcon from "../assets/search_FILL0_wght400_GRAD0_opsz48.svg";
+import ShopItem from "../components/ShopItem";
+import "../styles/Shop.css";
+
+import backpack from "../assets/backpack.png";
+import phoneCase from "../assets/phonecase.png";
+
 export default function Shop() {
-  return <div className="Shop">Shop Component</div>;
+  const items = [
+    { id: 0, title: "Backpack", price: "$39.50", img: backpack },
+    { id: 1, title: "Phone case", price: "$12.50", img: phoneCase },
+  ];
+
+  const list = items.map(function (item) {
+    return (
+      <ShopItem
+        key={item.id}
+        id={item.id}
+        title={item.title}
+        price={item.price}
+        img={item.img}
+      />
+    );
+  });
+
+  return (
+    <div className="Shop">
+      <div className="title">
+        <h1>Shop</h1>
+        <div className="search">
+          <input type="text"></input>
+          <img src={searchIcon} alt="search" />
+        </div>
+      </div>
+
+      <div className="item-container">{list}</div>
+    </div>
+  );
 }
